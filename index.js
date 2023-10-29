89e5bd3e68a918d4b230084e1d3e687e16f7c1bf
hadil <hihijazi@gmail.com>
2023-10-28 21:26:46 -0400

    save changes made in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Hadil Hijazi",
+    streetAddress: "123 Main St"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "age", 28);
+console.log(employee);
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    if (newEmployee.hasOwnProperty(key)) {
+        delete newEmployee[key];
+    }
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
